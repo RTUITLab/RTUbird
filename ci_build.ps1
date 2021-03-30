a3145a6a5c2fa18e504e816f2e7aa7c0cf44015a
Maksim <maksalmak@gmail.com>
2021-03-30 15:02:50 +0000

    Ignore folder before removing

--- ci_build.ps1
+++ ci_build.ps1
@@ -4,6 +4,6 @@ npm run build
 
 Set-Location ../WebClient
 npm ci
-Remove-Item deploy/build -Force -Recurse
+Remove-Item deploy/build -Force -Recurse -ErrorAction Ignore
 npm run build
 Copy-Item build/ -Destination deploy/build/ -Recurse -Force
